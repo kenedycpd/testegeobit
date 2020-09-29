b8ec42850143d3621e2f6549c154a8b9beeea839
kenedy <kenedycelestino89@gmail.com>
2020-09-29 16:23:28 -0300

    Test create

--- autor/models.py
+++ autor/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField('Nome', max_length=100, blank=False)
+    idade = models.IntegerField('idade')
+    email = models.EmailField('email')
+    telefone = models.CharField('telefone', max_length=14)
 
     class Meta:
         verbose_name = 'Autor'
--- autor/tests.py
+++ autor/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Autor
 
-# Create your tests here.
+
+class AutorTestCase(TestCase):
+    def setUp(self):
+        Autor.objects.create(
+            nome='kenedy',
+            idade=34,
+            email='kenedy@santos.com',
+            telefone=91343877
+
+        )
+
+    def test_retorna_str(self):
+        p1 = Autor.objects.get(nome='kenedy')
+        idade = 34
+        email = 'kenedy@santos.com'
+        telefone = 91343877
+
+        self.assertEqual(p1.__str__(), 'kenedy')
+        self.assertEqual(idade, 34)
+        self.assertEqual(email, 'kenedy@santos.com')
+        self.assertEqual(telefone, 91343877)
