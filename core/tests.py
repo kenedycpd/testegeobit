1bcab4cf4a0c34a8117be5c02f44d5e605623a72
kenedy <kenedycelestino89@gmail.com>
2020-09-30 10:00:34 -0300

    test core

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_home(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template_used(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+
+    def teste_link_home(self):
+        self.assertContains(self.response, 'href="#"')
--- livro/tests.py
+++ livro/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .forms import LivroForm
 
-# Create your tests here.
+
+class TestLivroForm(TestCase):
+    def setUp(self):
+        self.form = LivroForm()
+        self.resp = self.client.get('livro')
+
+    def test_form_livro(self):
+        esperado = ['titulo', 'autor', 'paginas', 'preco', 'data']
+        self.assertSequenceEqual(esperado, list(self.form.fields))
